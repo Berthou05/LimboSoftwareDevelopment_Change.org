a1ae93ea9e25c7ab9f809239bf45a4bfc8351309
Alexis Berthou <alexisberthou05@gmail.com>
2026-03-26 12:34:21 -0600

    [FEAT] Home view distribution and bare back

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -4,8 +4,24 @@ Last modification: March 24,2026
 Modified by: Hurtado, R.
 */
 
+const EmployeeTeam = require('../../models/employeeTeamMembership');
+const Employee = require('../../models/employee');
+const Team = require('../../models/team');
+const Project = require('../../models/project');
+const Activity = require('../../models/activity');
+
+/* getHome()
+Function responsible for rendering the home page with the latest activity sections.
+*/
+
 exports.getHome = (request, response, next) => {
-    return response.render('pages/home',{
-        csrfToken: request.csrfToken(),
-    });
+    const employeeId = request.session.employeeId;
+    
+    Employee.fetchById(employeeId).then(([employee]) => {
+            
+
+
+        return response.render('pages/home',{
+            csrfToken: request.csrfToken(),
+        });
 };
