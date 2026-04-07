6369ff61637c5cd2f06eed2ef0f301c70075891e
RodrigoHTEC10 <a01713854@tec.mx>
2026-04-07 12:55:05 -0600

    [FEAT]: Completion of ADMIN-05. Addition of change account status through AJAX.

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -11,12 +11,14 @@ const isAuth = require('../../middleware/isAuthenticated');
 const adminController = require('./admin.controller');
 
 // ADMIN-01: access administration views.
+router.post('/accounts/:account_id/role',isAuth, isAuth.requirePermission('ADMIN-05'), adminController.assignRole);
+router.post('/accounts/:account_id/status',isAuth, isAuth.requirePermission('ADMIN-05'), adminController.assignStatus);
 router.get('/accounts', isAuth, isAuth.requirePermission('ADMIN-01'), adminController.getAccounts);
 router.get('/roles', isAuth, isAuth.requirePermission('ADMIN-01'), adminController.getRoleAdmin);
 // ADMIN-03: delete roles.
 router.delete('/roles/:roleId', isAuth, isAuth.requirePermission('ADMIN-03'), adminController.deleteRole);
 // ADMIN-02: create roles.
 router.get('/roles/newRole/:name', isAuth, isAuth.requirePermission('ADMIN-02'), adminController.createRole);
-router.post('/accounts/:account_id/role',isAuth, isAuth.requirePermission('ADMIN-05'), adminController.assignRole);
+
 
 module.exports = router;
