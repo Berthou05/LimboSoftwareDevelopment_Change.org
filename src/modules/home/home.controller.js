9c5674c615b618fc91803a47191c87656965711c
A01662743 <a01662743@tec.mx>
2026-04-15 15:06:45 -0600

    [CHORE] stablish account and report subject correct communication for HOME

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -1,21 +1,32 @@
 const Project = require('../../models/project');
 const Report = require('../../models/report');
-const Employee = require('../../models/employee'); 
+const Employee = require('../../models/employee');
+const Account = require('../../models/account');
 const Team = require('../../models/team');
 
 exports.getHome = async (request, response, next) => {
-    const userId = request.session.user.id;
+    const sessionUser = request.session.user || {};
+    const userId = sessionUser.id;
+    const employeeId = sessionUser.employeeId;
 
     try {
         const [userProjects] = await Project.fetchByEmployeeId(userId);
         const [otherProjects] = await Project.fetchNotByEmployeeId(userId);
         const allVisibleProjects = [...userProjects, ...otherProjects];
 
+        const [accountRows] = userId ? await Account.fetchById(userId) : [[]];
+        const [employeeRows] = employeeId ? await Employee.getNamesByEmployeeId(employeeId) : [[]];
+
+        const accountInfo = accountRows[0] || {};
+        const employeeInfo = employeeRows[0] || {};
+
+        const displayName = employeeInfo.names || sessionUser.username || 'Usuario';
+
         return response.render('pages/home', {
             csrfToken: request.csrfToken(),
             pageTitle: 'Home',
             path: '/home',
-            user: request.session.user,
+            user: sessionUser,
             reportSubjects: {
                 employees: [],
                 teams: [],
@@ -23,13 +34,15 @@ exports.getHome = async (request, response, next) => {
             },
             myProjects: userProjects,
             latestReports: {},
-
-            // Exponer explícitamente para el view
-            account: request.session.user,   // para account.image
-            employee: request.session.user,  // para employee.names
+            account: {
+                image: accountInfo.image || `https://ui-avatars.com/api/?name=${encodeURIComponent(displayName)}&background=fbfbfe&color=1f2937`,
+            },
+            employee: {
+                names: displayName,
+            },
         });
     } catch (error) {
-        console.log("Error en Home Controller:", error);
+        console.log('Error en Home Controller:', error);
         next(error);
     }
 };
