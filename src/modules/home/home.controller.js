7c5a98e1ddbe7779ac3339f93b1f525b74612586
Alexis Berthou <alexisberthou05@gmail.com>
2026-04-16 20:27:05 -0600

    [FEAT]: add projects section to home

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -4,6 +4,9 @@ const Employee = require('../../models/employee');
 const Account = require('../../models/account');
 const Team = require('../../models/team');
 const Activity = require('../../models/activity');
+const Goal = require('../../models/goal');
+const Achievement = require('../../models/achievement');
+const Highlight = require('../../models/highlight');
 
 const formatDayLabel = function formatDayLabel(value) {
     const date = new Date(value);
@@ -62,8 +65,8 @@ exports.getHome = async (request, response, next) => {
             [activityRows],
             [teamRows],
         ] = await Promise.all([
-            Project.fetchByEmployeeId(userId),
-            Project.fetchNotByEmployeeId(userId),
+            employeeId ? Project.fetchByEmployeeId(employeeId) : Promise.resolve([[]]),
+            employeeId ? Project.fetchNotByEmployeeId(employeeId) : Promise.resolve([[]]),
             userId ? Account.fetchById(userId) : Promise.resolve([[]]),
             employeeId ? Employee.getNamesByEmployeeId(employeeId) : Promise.resolve([[]]),
             employeeId ? Activity.fetchByEmployee(employeeId) : Promise.resolve([[]]),
@@ -81,6 +84,35 @@ exports.getHome = async (request, response, next) => {
             ? await Activity.getTeamMembersActivities(primaryTeam.team_id)
             : [[]];
         const teamActivitySections = buildActivitySections(teamActivityRows.slice(0, 10));
+        const projectPanels = await Promise.all(userProjects.map(async (project) => {
+            const [goalRows] = await Goal.fetchByProject(project.project_id);
+            const [achievementRows] = await Achievement.fetchByProject(project.project_id);
+            const [highlightRows] = await Highlight.fetchByProject(project.project_id);
+
+            return {
+                id: project.project_id,
+                name: project.name || 'Unnamed project',
+                status: project.status || 'Unknown',
+                startDate: project.start_date || null,
+                description: project.description || '',
+                goals: goalRows.map((goal) => ({
+                    title: goal.title || 'Untitled goal',
+                    description: goal.description || '',
+                    dueDate: goal.due_date || null,
+                    status: goal.status || 'UNKNOWN',
+                })),
+                achievements: achievementRows.map((achievement) => ({
+                    title: achievement.title || 'Untitled achievement',
+                    description: achievement.description || '',
+                    achievementDate: achievement.achievement_date || null,
+                })),
+                highlights: highlightRows.map((highlight) => ({
+                    title: highlight.title || 'Untitled highlight',
+                    content: highlight.content || '',
+                    createdAt: highlight.created_at || null,
+                })),
+            };
+        }));
         const displayName = employeeInfo.names || sessionUser.username || 'Usuario';
 
         return response.render('pages/home', {
@@ -97,6 +129,7 @@ exports.getHome = async (request, response, next) => {
             latestReports: {},
             latestActivitySections,
             teamActivitySections,
+            projectPanels,
             account: {
                 image: accountInfo.image || `https://ui-avatars.com/api/?name=${encodeURIComponent(displayName)}&background=fbfbfe&color=1f2937`,
             },
