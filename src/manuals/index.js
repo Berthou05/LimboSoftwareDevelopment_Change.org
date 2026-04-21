2e957d98ff264626cbb40b8f258a3f752433fa05
Alexis Berthou <alexisberthou05@gmail.com>
2026-04-21 12:00:59 -0600

    [FIX]: restore manual library structure

--- src/manuals/index.js
+++ src/manuals/index.js
@@ -1,7 +1,20 @@
 const quickStart = require('./quickStart');
-const modules = require('./modules');
+const projects = require('./projects/projects');
+const roles = require('./admin/roles');
+const dailyEntries = require('./dailyEntry/dailyEntries');
+const reports = require('./reports/reports');
 
 module.exports = [
     quickStart,
-    modules,
+    {
+        key: 'modules',
+        title: 'Workspace guides',
+        description: 'Task-focused guides grouped by module instead of duplicating each CRUD action.',
+        entries: [
+            projects,
+            roles,
+            dailyEntries,
+            reports,
+        ],
+    },
 ];
