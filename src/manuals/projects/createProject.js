a08641b8f023f2fc4667c18d8d4c89cf5fb57663
Alexis Berthou <alexisberthou05@gmail.com>
2026-04-21 11:48:46 -0600

    [STYLE] Changed the icon for informatio sign

--- src/manuals/projects/createProject.js
+++ src/manuals/projects/createProject.js
@@ -6,19 +6,10 @@ module.exports = {
     helpKey: 'quick-start.create-project',
     steps: [
         {
-            title: 'Go to the Project page',
-            body: 'Go to the Project page where all available projects are present.',
-            image: {
-                src: '/images/manuals/Projects/create-project1.webp',
-                alt: 'Project page with all available projects visible',
-                caption: 'Open the Project page before starting the creation flow.',
-            },
-        },
-        {
             title: 'Click Create Project',
             body: 'Click on the `Create Project` button in the upper right corner.',
             image: {
-                src: '/images/manuals/Projects/create-project2.webp',
+                src: '/images/manuals/Projects/create-project1.webp',
                 alt: 'Create Project button in the project directory',
                 caption: 'Use the create action in the upper right corner of the Project page.',
             },
@@ -27,7 +18,7 @@ module.exports = {
             title: 'Fill the New Project form',
             body: 'Fill the `New Project` form. Only Name and Start Date are required fields.',
             image: {
-                src: '/images/manuals/Projects/create-project3.webp',
+                src: '/images/manuals/Projects/create-project2.webp',
                 alt: 'New Project form with the required fields visible',
                 caption: 'Name and Start Date are the only required fields.',
             },
@@ -36,7 +27,7 @@ module.exports = {
             title: 'Review the form before saving',
             body: 'You can edit the fields later. Click on the `Cancel` button anytime you want to cancel the process.',
             image: {
-                src: '/images/manuals/Projects/create-project4.webp',
+                src: '/images/manuals/Projects/create-project3.webp',
                 alt: 'New Project form showing the cancel option',
                 caption: 'Use Cancel to leave the process without creating the project.',
             },
@@ -45,7 +36,7 @@ module.exports = {
             title: 'Create the project',
             body: 'Click on the `Create Project` button. You will be sent to the Projects page and your new project will be available in `My Projects` section.',
             image: {
-                src: '/images/manuals/Projects/create-project5.webp',
+                src: '/images/manuals/Projects/create-project4.webp',
                 alt: 'Projects page showing the new project in My Projects',
                 caption: 'After saving, the new project appears in the My Projects section.',
             },
