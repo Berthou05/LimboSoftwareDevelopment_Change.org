af6e09d780a2d63ef7b91d7a181c61e2817d0317
Alexis Berthou <alexisberthou05@gmail.com>
2026-04-21 12:06:12 -0600

    [DOCS]: restructure quick start manuals

--- src/manuals/quickStart.js
+++ src/manuals/quickStart.js
@@ -1,13 +1,15 @@
+const systemOverview = require('./systemOverview');
 const createProject = require('./projects/createProject');
 const submitDailyStandup = require('./dailyEntry/submitDailyStandup');
 const generateReport = require('./reports/generateReport');
-const assignTeamMember = require('./team');
+const assignTeamMember = require('./team/assignTeamMember');
 
 module.exports = {
     key: 'quick-start',
     title: 'Quick Start',
-    description: 'First-use guides for the most common setup tasks a new user completes in the application.',
+    description: 'Onboarding guides for the first actions a new user performs in the system.',
     entries: [
+        systemOverview,
         createProject,
         submitDailyStandup,
         generateReport,
